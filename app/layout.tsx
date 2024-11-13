2e3e5fbf5a6b08b1130259f793d8a3f5698bdc77
uabdirasul <ubbiniyazov04@gmail.com>
2024-11-13 16:29:30 +0500

    fix issue about ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { source_code_pro_font } from "@/app/ui/fonts";
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
-import Head from "next/head";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -17,25 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
+      <head>{/* Meta and title settings handled by Next.js metadata */}</head>
+      <body className={`${source_code_pro_font.className} antialiased`}>
+        <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        ></script>
-        <script
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-Y10H128YC7');
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
             `
           }}
-        ></script>
-      </Head>
-
-      <body className={`${source_code_pro_font.className} antialiased`}>
+        />
         <Navbar />
         {children}
       </body>
